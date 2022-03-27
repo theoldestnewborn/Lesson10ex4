95fd142f481c653f3f9d5bf8ef346ddbe5b97660
Aleksei <iamviga@yandex.ru>
2022-03-27 21:32:14 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Введите слова");
+        Scanner sc1 = new Scanner(System.in);
+        String words = sc1.nextLine();
+        String[] xArray = words.split(" ");
+        System.out.println("Какое слово проверить");
+        String word = sc1.next();
+        StringBuilder xBuilder = new StringBuilder(word);
+        String x1 = String.valueOf(xBuilder.reverse());
+        if (word.equals(x1)) {
+            System.out.println("У нас палиндромище!");
+        } else {
+            System.out.println("попробуй другое слово");
+        }
+    }
+}
